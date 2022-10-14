18e8ddadfa036ba60988c916145048c1b8b2f410
GeorgeKusenkov <g-jazzman@yandex.ru>
2022-10-14 18:58:32 +0500

    Initial commit

---
+++ app/src/main/java/com/example/geoposition/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.geoposition
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricManager.Strings
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.geoposition.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedClient: FusedLocationProviderClient
+
+    private val launcher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { map ->
+        if(map.values.isNotEmpty() && map.values.all {it}) {
+            startLocation()
+        }
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            binding.message.text = p0.lastLocation.toString()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocation() {
+            val request = LocationRequest.create()
+                .setInterval(1000)
+                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+        
+        fusedClient.requestLocationUpdates(
+            request,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkPermissions()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        fusedClient.removeLocationUpdates(locationCallback)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fusedClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun checkPermissions() {
+        if(REQUIRED_PERMISSIONS.all { permission ->
+                ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+            }) {
+            startLocation()
+        } else {
+            launcher.launch(REQUIRED_PERMISSIONS)
+        }
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+    }
+}
